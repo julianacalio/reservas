2e2d03973036c3a58a7cae1e62a0f26626df62f4
charlesportoferreira <charlesportoferreira@gmail.com>
2014-01-04 14:34:09 -0200

    tentativa de colocar c3p0 no hibernate

--- src/java/facade/AbstractFacade.java
+++ src/java/facade/AbstractFacade.java
@@ -8,23 +8,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Projections;
 
-
 public abstract class AbstractFacade<T> {
 
     private final Class<T> entityClass;
 
-  
-
-    
-        public AbstractFacade(Class<T> entityClass) {
+    public AbstractFacade(Class<T> entityClass) {
         this.entityClass = entityClass;
     }
 
-   
     protected abstract SessionFactory getSessionFactory();
 
     /**
      * Salva uma entidade no banco de dados
+     *
      * @param entity Entidade a ser salva
      */
     public void save(T entity) {
@@ -40,6 +36,7 @@ public abstract class AbstractFacade<T> {
 
     /**
      * Atualiza uma entidade no banco de dados.
+     *
      * @param entity Entidade a ser atualizada
      */
     public void edit(T entity) {
@@ -54,9 +51,9 @@ public abstract class AbstractFacade<T> {
     }
 
     /**
-     * Combina os dados de uma entidade no banco de dados
-     * e retorna esta entidade atualizada. Caso não exista
-     * esta entidade, uma nova é criada.
+     * Combina os dados de uma entidade no banco de dados e retorna esta
+     * entidade atualizada. Caso não exista esta entidade, uma nova é criada.
+     *
      * @param entity Entidade a ser combinada
      * @return Entidade atualizada/criada
      */
@@ -74,6 +71,7 @@ public abstract class AbstractFacade<T> {
 
     /**
      * Remove uma entidade do banco de dados.
+     *
      * @param entity Entidade do banco de dados a ser removida
      */
     public void remove(T entity) {
@@ -89,6 +87,7 @@ public abstract class AbstractFacade<T> {
 
     /**
      * Busca uma entidade no banco de dados de acordo com o id.
+     *
      * @param id ID da entidade a ser busca
      * @return
      */
@@ -101,6 +100,7 @@ public abstract class AbstractFacade<T> {
 
     /**
      * Busca todas as entidades do banco de dados.
+     *
      * @return Lista com todas as entidades do banco de dados
      */
     public List<T> findAll() {
@@ -114,8 +114,8 @@ public abstract class AbstractFacade<T> {
     }
 
     /**
-     * Busca todas as entidades do banco de dados dentro de um
-     * range específico.
+     * Busca todas as entidades do banco de dados dentro de um range específico.
+     *
      * @param range Vetor representando o range de dados.
      * @return Lista com todas as entidades do banco de dados
      */
@@ -132,6 +132,7 @@ public abstract class AbstractFacade<T> {
 
     /**
      * Conta quanto itens dessa entidade possui o banco de dados.
+     *
      * @return A quantidade de itens que possui o banco
      */
     public int count() {
